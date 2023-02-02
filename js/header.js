4a9b86d67c719854779b3bff8182334208153489
Dev <jaiswaldevraj337@gmail.com>
2023-02-02 15:16:26 +0530

    real state hero completed

--- js/header.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function () {
-   $("#hero-section").load("hero.html");
-    $("#footer").load("footer.html");
-    $("#index").load("index.html");
-});
---
+++ js/real-state.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  $("#header-section").load("real-state-header.html");
+  $("#footer-section").load("footer.html");
+});
