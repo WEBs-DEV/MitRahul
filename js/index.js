08768f80334b2cb40547c3ccdba0780c6a35e38d
Dev <jaiswaldevraj337@gmail.com>
2023-02-03 13:19:09 +0530

    home section updated

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#header-section").load("header.html");
+    $("#card-slider-section").load("card-slider.html");
+    $("#footer-section").load("footer.html");
+  });
+  
