01aeab48352365285e6d7cf8ed00f4b76cce0060
Dev <jaiswaldevraj337@gmail.com>
2023-02-07 15:10:46 +0530

    home page updated

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $("#header-section").load("header.html");
+    $("#counter-number-section").load("counter-number.html");
     $("#footer-section").load("footer.html");
   });
   
+
+// aos animation scroll initializing for on load scrolling
+AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 200, // offset (in px) from the original trigger point
+    // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+});
--- js/real-state.js
+++ js/real-state.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   $("#header-section").load("real-state-header.html");
+  $("#photo-gallery-section").load("photo-gallery.html");
   $("#footer-section").load("footer.html");
 });
