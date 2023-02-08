8541b14f6258a1553d81eaa71c0aaa33b6bb90a3
Dev <jaiswaldevraj337@gmail.com>
2023-02-08 12:54:50 +0530

    photo gallery images changed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
+
+$("#header-section").load("header.html");
+
 $(document).ready(function () {
-    $("#header-section").load("header.html");
+    
     $("#counter-number-section").load("counter-number.html");
     $("#footer-section").load("footer.html");
   });
@@ -8,8 +11,9 @@ $(document).ready(function () {
 // aos animation scroll initializing for on load scrolling
 AOS.init({
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 200, // offset (in px) from the original trigger point
+    offset: 150, // offset (in px) from the original trigger point
     // values from 0 to 3000, with step 50ms
+    delay:1000,
     duration: 1000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
 });
--- js/real-state.js
+++ js/real-state.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
   $("#photo-gallery-section").load("photo-gallery.html");
   $("#footer-section").load("footer.html");
 });
+
+// aos animation scroll initializing for on load scrolling
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 50, // offset (in px) from the original trigger point
+  // values from 0 to 3000, with step 50ms
+  delay:1000,
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+});
